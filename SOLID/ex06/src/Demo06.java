80bc783d31a3512dbe37f170c292d27fad9a41e4
Vinayak Paka <vinayakpaka22@gmail.com>
2025-08-25 11:25:25 +0530

    Solved q6

--- SOLID/ex06/src/Demo06.java
+++ SOLID/ex06/src/Demo06.java
@@ -1,6 +1,24 @@
 public class Demo06 {
     public static void main(String[] args) {
-        new Aviary().release(new Bird());
-        new Aviary().release(new Penguin()); // runtime error
+        Aviary aviary = new Aviary();
+        
+        // Demo the aviary with flyable birds
+        System.out.println("=== Simple SOLID Solution ===");
+        
+        Bird bird = new Bird();
+        aviary.release(bird); // This works fine
+        
+        // Penguin can't fly, so it's not used in aviary
+        Penguin penguin = new Penguin();
+        System.out.println("\n--- Penguin activities ---");
+        penguin.walk();
+        penguin.swim();
+        
+        // Try to use penguin in aviary (this would cause compile error, which is good!)
+        // aviary.release(penguin); // Compile error - Penguin doesn't implement Flyable
+        
+        System.out.println("\n--- Different bird types ---");
+        Bird sparrow = new Bird();
+        aviary.release(sparrow);
     }
 }
--- SOLID/ex06/src/Penguin.java
+++ SOLID/ex06/src/Penguin.java
@@ -1,5 +1,9 @@
-public class Penguin extends Bird {
-    @Override void fly(){
-         throw new UnsupportedOperationException("Can't fly"); 
+public class Penguin {
+    public void swim() {
+        System.out.println("Swimming!");
+    }
+    
+    public void walk() {
+        System.out.println("Waddling!");
     }
 }
