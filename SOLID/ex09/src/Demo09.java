316a9c7548b34f75c040135ba665de1bc98441d7
Vinayak Paka <vinayakpaka22@gmail.com>
2025-08-25 11:44:22 +0530

    Solved q9

--- SOLID/ex09/src/Demo09.java
+++ SOLID/ex09/src/Demo09.java
@@ -1,5 +1,24 @@
 public class Demo09 {
     public static void main(String[] args) {
-        new OrderController().create("ORD-1");
+        System.out.println("=== Simple SOLID Solution ===");
+        
+        // Using SQL repository
+        OrderRepository sqlRepo = new SqlOrderRepository();
+        OrderController sqlController = new OrderController(sqlRepo);
+        sqlController.create("ORD-1");
+        
+        // Using in-memory repository
+        System.out.println("\n--- Different repository ---");
+        OrderRepository memoryRepo = new InMemoryOrderRepository();
+        OrderController memoryController = new OrderController(memoryRepo);
+        memoryController.create("ORD-2");
+        
+        // Easy to swap repositories
+        System.out.println("\n--- Swapping repositories ---");
+        OrderController controller = new OrderController(sqlRepo);
+        controller.create("ORD-3");
+        
+        controller = new OrderController(memoryRepo);
+        controller.create("ORD-4");
     }
 }
