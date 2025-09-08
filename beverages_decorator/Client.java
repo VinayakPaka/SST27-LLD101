757162352af21727c8245af132beb9ac7168b122
Vinayak Paka <vinayakpaka22@gmail.com>
2025-09-08 18:56:34 +0530

    Solved

--- beverages_decorator/Client.java
+++ beverages_decorator/Client.java
@@ -3,15 +3,12 @@ package beverages_decorator;
 public class Client {
 
 	public static void main(String[] args) {
-		
-
-		
-		Beverage coffee = new Cappuccino();
-        System.out.println("Coffee");
-		System.out.println(coffee.cost());
-		
-		
-
+		Beverage base = new Cappuccino();
+		System.out.println(base.cost());
+		Beverage withSugar = new SugarDecorator(base);
+		System.out.println(withSugar.cost());
+		Beverage withChoco = new ChocolateDecorator(withSugar);
+		System.out.println(withChoco.cost());
 	}
 
 }
